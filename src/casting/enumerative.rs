2632d42a4afaf793366bc7e915304f26e479112f
Michele Riva <michele.riva@oramasearch.com>
2025-09-24 16:01:13 +0200

    improves memory management

--- src/casting/enumerative.rs
+++ src/casting/enumerative.rs
@@ -19,173 +19,102 @@
 /// Enumerative parser usage:
 ///
 /// ```rs
-/// let result = Enumerative::parse("enum(\"engineer\")");
-/// assert!(result.is_enum);
-/// assert_eq!(result.enum_value, Some("engineer".to_string()));
+/// let result = parse("enum(\"engineer\")");
+///
+/// assert_eq!(result, Ok(Some("engineer".to_string()));
 /// ```
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum EnumerativeParserError {
     #[error(
         "Parse error: Expected single ('), double (\") or backtick (`) quote at position {0}. Found \"{1}\" instead."
     )]
     InvalidQuoteChar(usize, char),
-    #[error(
-        "Parse error: Expected closing parenthesis at the end of enum call. Found \"{0}\" instead."
-    )]
-    MissingCloseParenthesis(char),
+
+    #[error("Parse error: Expected closing parenthesis at the end of enum call.")]
+    MissingCloseParenthesis,
+
     #[error(
         "Parse error: Mismatched closing quote for enum value. Expected \" {0} \" but found \" {1} \"."
     )]
     MismatchedClosingQuote(char, char),
+
+    #[error("Parse error: Missing closing quote character due to escape")]
+    MissingClosingCharacter,
+
+    #[error("Missin enum value")]
+    MissingEnumValue,
 }
 
-static ESCAPABLE_CHARS: [char; 4] = ['\'', '"', '`', ')'];
-
-#[derive(Clone, Debug, PartialEq)]
-enum QuoteStyle {
-    Single,
-    Double,
-    Backtick,
-}
-
-impl Into<char> for QuoteStyle {
-    fn into(self) -> char {
-        match self {
-            QuoteStyle::Single => '\'',
-            QuoteStyle::Double => '"',
-            QuoteStyle::Backtick => '`',
-        }
+pub fn parse(value: &str) -> Result<Option<String>, EnumerativeParserError> {
+    // Early exit in case it doesn't start with "enum(".
+    if !value.starts_with("enum(") {
+        return Ok(None);
     }
-}
 
-#[derive(Clone, Debug)]
-pub struct Enumerative<'input> {
-    pub is_enum: bool,
-    pub enum_value: Option<String>,
+    // If the last character is not a closing parenthesis, it's an error.
+    if !value.ends_with(")") {
+        return Err(EnumerativeParserError::MissingCloseParenthesis);
+    }
 
-    raw_value: &'input str,
-    chars: Vec<char>,
-    cursor: usize,
-    inside_enum: bool,
-    quote_char: Option<QuoteStyle>,
-}
+    // If the second to last character is not a matching quote, it's an error.
+    let maybe_closing_quote: &str = &value[5..value.len() - 1];
+    let mut maybe_closing_quote_chars = maybe_closing_quote.chars();
 
-impl<'input> Enumerative<'input> {
-    pub fn parse(value: &'input str) -> Result<Self, EnumerativeParserError> {
-        let mut parser = Enumerative {
-            is_enum: false,
-            enum_value: None,
-            raw_value: value.trim(),
-            chars: value.chars().collect(),
-            cursor: 0,
-            inside_enum: false,
-            quote_char: None,
+    let first_quote_char = maybe_closing_quote_chars.next();
+    let last_quote_char = maybe_closing_quote_chars.last();
+
+    if first_quote_char != last_quote_char {
+        dbg!(&first_quote_char, &last_quote_char);
+
+        return Err(EnumerativeParserError::MismatchedClosingQuote(
+            maybe_closing_quote.chars().next().unwrap_or(' '),
+            maybe_closing_quote.chars().last().unwrap_or(' '),
+        ));
+    }
+
+    let quote_char = if let Some(first_char) = first_quote_char {
+        static ALLOWED_QUOTE_CHARS: [char; 3] = ['"', '\'', '`'];
+
+        if !ALLOWED_QUOTE_CHARS.contains(&first_char) {
+            return Err(EnumerativeParserError::InvalidQuoteChar(0, first_char));
+        }
+
+        first_char
+    } else {
+        // case: "enum()"
+        return Err(EnumerativeParserError::MissingEnumValue);
+    };
+
+    // enum("...
+    // so it's time to start reading and accumulating the enum value.
+    let escaped_quote = &maybe_closing_quote[1..maybe_closing_quote.len() - 1];
+    let mut enum_value = String::with_capacity(escaped_quote.len());
+    let mut characters = escaped_quote.chars();
+
+    loop {
+        let Some(current_char) = characters.next() else {
+            break;
         };
 
-        // Early exit in case it doesn't start with "enum(".
-        if !parser.raw_value.starts_with("enum(") {
-            return Ok(parser.clone());
-        }
+        if current_char == '\\' {
+            let Some(next_char) = characters.next() else {
+                return Err(EnumerativeParserError::MissingClosingCharacter);
+            };
 
-        parser.inside_enum = true;
-        parser.cursor += 5; // Move past "enum("
-        parser.quote_char = parser.peek_quote_char_at(None);
-
-        // The first character of an enum call should be either a single, double, or backtick quote.
-        if parser.quote_char.is_none() {
-            return Err(EnumerativeParserError::InvalidQuoteChar(
-                parser.cursor,
-                *parser.chars.get(parser.cursor).unwrap_or(&' '),
-            ));
-        }
-
-        // If the last character is not a closing parenthesis, it's an error.
-        if parser.chars.last() != Some(&')') {
-            return Err(EnumerativeParserError::MissingCloseParenthesis(
-                *parser.chars.last().unwrap_or(&' '),
-            ));
-        }
-
-        // If the second to last character is not a matching quote, it's an error.
-        let last_but_one = parser.chars.len().checked_sub(2);
-        if parser.peek_quote_char_at(last_but_one) != parser.quote_char {
-            return Err(EnumerativeParserError::MismatchedClosingQuote(
-                parser
-                    .quote_char
-                    .clone()
-                    .unwrap_or(QuoteStyle::Double)
-                    .into(), // @todo: we should never be in the 'or' condition of this unwrap.
-                parser
-                    .peek_quote_char_at(last_but_one)
-                    .unwrap_or(QuoteStyle::Double)
-                    .into(),
-            ));
-        }
-
-        // If no error is found, we can safely assume we're inside an enum call.
-        parser.inside_enum = true;
-        // Move past the opening quote.
-        parser.cursor += 1;
-
-        // Cursor now should be in position 6:
-        // enum("...
-        // so it's time to start reading and accumulating the enum value.
-        let mut enum_value = String::new();
-
-        while parser.cursor < parser.chars.len() {
-            let current_char = parser.chars[parser.cursor];
-            dbg!("Current char: {}", current_char);
-
-            // Current char could be an escaped quote, parenthesis, or backlash.
-            if current_char == '\\' {
-                let next_char = parser.chars.get(parser.cursor + 1);
-                if ESCAPABLE_CHARS.contains(next_char.unwrap_or(&' ')) {
-                    enum_value.push(*next_char.unwrap());
-                    parser.cursor += 2; // Skip the escape character and the escaped character.
-                    continue;
-                } else {
-                    enum_value.push(current_char);
-                    parser.cursor += 1;
-                    continue;
-                }
+            if next_char == quote_char {
+                enum_value.push(next_char);
+            } else {
+                enum_value.push(current_char);
+                enum_value.push(next_char);
             }
-
-            // If we find the closing quote, we should stop accumulating.
-            if let Some(quote_style) = &parser.quote_char {
-                let is_closing_quote = match quote_style {
-                    QuoteStyle::Single => current_char == '\'',
-                    QuoteStyle::Double => current_char == '"',
-                    QuoteStyle::Backtick => current_char == '`',
-                };
-                if is_closing_quote {
-                    break;
-                }
-            }
-
-            parser.cursor += 1;
+        } else {
             enum_value.push(current_char);
         }
-
-        Ok(Enumerative {
-            is_enum: true,
-            enum_value: Some(enum_value),
-            ..parser.clone()
-        })
     }
 
-    #[inline]
-    fn peek_quote_char_at(&self, position: Option<usize>) -> Option<QuoteStyle> {
-        let idx = position.unwrap_or(self.cursor);
-
-        match self.chars.get(idx) {
-            Some('"') => Some(QuoteStyle::Double),
-            Some('\'') => Some(QuoteStyle::Single),
-            Some('`') => Some(QuoteStyle::Backtick),
-            _ => None,
-        }
-    }
+    Ok(Some(enum_value))
 }
 
 #[cfg(test)]
@@ -194,76 +123,71 @@ mod tests {
 
     #[test]
     fn simple_enum_parsing() {
-        let result = Enumerative::parse("enum(\"engineer\")").unwrap();
-        assert!(result.is_enum);
-        assert_eq!(result.enum_value, Some("engineer".to_string()));
+        let result = parse("enum(\"engineer\")");
+        assert_eq!(result, Ok(Some("engineer".to_string())));
     }
 
     #[test]
     fn simple_enum_parsing_single_quotes() {
-        let result = Enumerative::parse("enum('engineer')").unwrap();
-        assert!(result.is_enum);
-        assert_eq!(result.enum_value, Some("engineer".to_string()));
+        let result = parse("enum('engineer')");
+        assert_eq!(result, Ok(Some("engineer".to_string())));
     }
 
     #[test]
     fn simple_enum_parsing_backtick_quotes() {
-        let result = Enumerative::parse("enum(`engineer`)").unwrap();
-        assert!(result.is_enum);
-        assert_eq!(result.enum_value, Some("engineer".to_string()));
+        let result = parse("enum(`engineer`)");
+        assert_eq!(result, Ok(Some("engineer".to_string())));
     }
 
     #[test]
     fn enum_parsing_with_escaped_quotes() {
-        let result = Enumerative::parse("enum(\"engi\\\"neer\")").unwrap();
-        assert!(result.is_enum);
-        assert_eq!(result.enum_value, Some("engi\"neer".to_string()));
+        let result = parse("enum(\"engi\\\"neer\")");
+
+        assert_eq!(result, Ok(Some("engi\"neer".to_string())));
     }
 
     #[test]
     fn enum_parsing_with_mixed_quotes() {
-        let result = Enumerative::parse("enum('engi`neer')").unwrap();
-        assert!(result.is_enum);
-        assert_eq!(result.enum_value, Some("engi`neer".to_string()));
+        let result = parse("enum('engi`neer')");
+
+        assert_eq!(result, Ok(Some("engi`neer".to_string())));
     }
 
     #[test]
     fn enum_parsing_with_escaped_backslash() {
-        let result = Enumerative::parse("enum(\"engi\\neer\")").unwrap();
-        assert!(result.is_enum);
-        assert_eq!(result.enum_value, Some("engi\\neer".to_string()));
+        let result = parse("enum(\"engi\\neer\")");
+        assert_eq!(result, Ok(Some("engi\\neer".to_string())));
     }
 
     // Remember: if it doesn't start with enum(, it's not an enum.
     // In that case, we do not expect an error.
     #[test]
     fn enum_parsing_missing_opening_parenthesis() {
-        let result = Enumerative::parse("enum\"engineer\")").unwrap();
-        assert!(!result.is_enum);
-        assert_eq!(result.enum_value, None);
+        let result = parse("enum\"engineer\")");
+        assert_eq!(result, Ok(None));
     }
 
     #[test]
     fn enum_parsing_missing_opening_quote() {
-        let result = Enumerative::parse("enum(engineer\")");
+        let result = parse("enum(engineer\")");
         assert!(result.is_err());
     }
 
     #[test]
     fn enum_parsing_missing_closing_parenthesis() {
-        let result = Enumerative::parse("enum(\"engineer\"");
+        let result = parse("enum(\"engineer\"");
         assert!(result.is_err());
     }
 
     #[test]
     fn enum_parsing_missing_closing_quote() {
-        let result = Enumerative::parse("enum(\"engineer)");
+        let result = parse("enum(\"engineer)");
         assert!(result.is_err());
     }
 
     #[test]
     fn enum_parsing_mismatched_closing_quote() {
-        let result = Enumerative::parse("enum(\"engineer')");
+        let result = parse("enum(\"engineer')");
         assert!(result.is_err());
     }
 }
